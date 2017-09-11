d05f5e193b2093a1dabe1f4a7deecac9f59c53df
Nipun Hewage <nth6@duke.edu>
2017-09-10 23:13:31 -0400

    initial commit

---
+++ resources/javascript/script.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+    /* Sticky Navigation*/
+   $('.js--section-about').waypoint(function(direction) { 
+    if (direction == "down") {
+        $('nav1').addClass('sticky');
+    } else {
+        $('nav1').removeClass('sticky');
+    }
+   }, 
+                                    {
+       offset: '50px;'
+   }); 
+                                    
+    
+    /*Scrolling */
+    $('.js--scroll-to-about').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-about').offset().top}, 700)
+      
+      });
+    
+    /* Navigation Scroll */
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+    
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 500;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});    
+    
+});
